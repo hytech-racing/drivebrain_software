2582e8c7aba23fcc5104e1e8bc0d93748407092c
Ben Hall <rcmast3r1@gmail.com>
2024-08-11 02:14:48 -0400

    adding design docs for parameter server integration

--- drivebrain_comms/foxglove_param_server/foxglove_param_server.md
+++ drivebrain_comms/foxglove_param_server/foxglove_param_server.md
@@ -1,6 +1,41 @@
 # parameter server
 
 
+what we want to be able to do here is construct the parameter server from all configuration-needed software components
+
+REQUIREMENTS:
+
+- The configuration interface will expose to the parameter server what parameters exist 
+
+- be able to re-save the final ptree :brain:? (this is something that ROS has lol)
+
+- the parameter server will also handle recording / tracking of all parameters changed and to what value
+
+## IMPLEMENTATION DETAILS
+
+on construction of the parameter server we give it a vector of each configurable-downcasted object. the parameter server looksat each of the options that the components contain and creates its vector of foxglove parameters with the correct types
+
+## NOTES
+the parameter server will also communicate with the mcap broadcaster? 
+- yes, and GRPC would be good for this I think for handling the handshake with `data_acq`
+    
+### considerations 
+
+why dont we handle the mcap recording within our software here?:
+- at this point why dont I just integrate the mcap recording into here?
+
+-------
+##### pros:
+- I wouldnt have to broadcast or handle deserialization / serialization of the protobuf messages out of the drivebrain core
+
+- I wouldnt have to implement a protocol to handle handshaking with an external 
+-------
+##### cons:  
+- I would have to then handle all the meta data handling stuff here too tho which would require alot more integration with foxglove studio since we would need request-response (service calls) that is already mostly implemented in the `data_acq` service. 
+       
+- I would also have to implement the request / response call for starting and stopping of recording.
+-------
+
 this component of the drivebrain will contain the foxglove parameter server implementation / intergration for handling the adjustment of internal parameters.
 
 ## TODO
--- test/test_param_server.cpp
+++ test/test_param_server.cpp
@@ -8,7 +8,6 @@
 #include <thread>
 #include <unordered_set>
 
-#include <foxglove/websocket/base64.hpp>
 #include <foxglove/websocket/server_factory.hpp>
 #include <foxglove/websocket/websocket_notls.hpp>
 #include <foxglove/websocket/websocket_server.hpp>
@@ -25,8 +24,10 @@ int main()
     foxglove::ServerOptions serverOptions;
     serverOptions.capabilities.push_back("parameters");
     serverOptions.capabilities.push_back("parametersSubscribe");
+
     auto server = foxglove::ServerFactory::createServer<websocketpp::connection_hdl>(
         "C++ Protobuf example server", logHandler, serverOptions);
+
     foxglove::Parameter test_param("test_param", "yo");
 
     foxglove::ServerHandlers<foxglove::ConnHandle> hdlrs;
@@ -36,29 +37,32 @@ int main()
     {
         server->publishParameterValues(clientHandle, {test_param}, request_id);
     };
-    hdlrs.parameterSubscriptionHandler = [&](const std::vector<std::string> &params_to_subscribe, 
-        foxglove::ParameterSubscriptionOperation op_to_perform,
-        foxglove::ConnHandle clientHandle) {
-        
-        std::cout <<"uh"<<std::endl;
+
+    hdlrs.parameterSubscriptionHandler = [&](const std::vector<std::string> &params_to_subscribe,
+                                             foxglove::ParameterSubscriptionOperation op_to_perform,
+                                             foxglove::ConnHandle clientHandle)
+    {
+        std::cout << "uh" << std::endl;
     };
 
-    hdlrs.parameterChangeHandler = [&](const std::vector<foxglove::Parameter>& params, const std::optional<std::string>& request_id, foxglove::ConnHandle clientHandle)
+    hdlrs.parameterChangeHandler = [&](const std::vector<foxglove::Parameter> &params, const std::optional<std::string> &request_id, foxglove::ConnHandle clientHandle)
     {
         test_param = params.at(0);
         server->publishParameterValues(clientHandle, {test_param}, request_id);
     };
+
     server->setHandlers(std::move(hdlrs));
     server->start("0.0.0.0", 8765);
 
     std::signal(SIGINT, [](int sig)
                 {
     std::cerr << "received signal " << sig << ", shutting down" << std::endl;
+
+
     running = false; });
 
     while (running)
     {
-
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
     };
     server->stop();
