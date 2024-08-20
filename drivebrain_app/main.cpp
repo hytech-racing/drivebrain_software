fb340e6013d718577a590d0823848e09c14b7b0f
Ben Hall <rcmast3r1@gmail.com>
2024-08-20 17:46:56 -0400

    adding in more TODOs

--- drivebrain_app/main.cpp
+++ drivebrain_app/main.cpp
@@ -1,7 +1,14 @@
 #include <DrivebrainBase.hpp>
 #include <SimpleController.hpp>
+#include <CANComms.hpp>
 
 
+// TODO first application will have
+// - [ ] driver bus https://chatgpt.com/share/03297278-0346-40ba-8ce7-7a75e919ee5c
+// - [ ] CAN driver
+// - [ ] simple controller
+// - [ ] vehicle manager
+// - [ ] foxglove live telem and parameter server 
 
 int main()
 {
--- drivebrain_core_impl/drivebrain_comms/car_CAN_driver/include/CANComms.hpp
+++ drivebrain_core_impl/drivebrain_comms/car_CAN_driver/include/CANComms.hpp
@@ -1,1 +1,14 @@
 #pragma once
+
+#include <linux/can.h>
+
+
+// TODO - [ ] be able to hook into the driver bus tx and tx queues
+
+namespace comms 
+{
+    class CANDriver
+    {
+
+    };
+}
