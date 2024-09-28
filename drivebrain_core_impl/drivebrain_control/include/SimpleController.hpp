72d20e46f259c009da25819b329937a83eafd023
Ben Hall <rcmast3r1@gmail.com>
2024-09-28 15:50:59 -0400

    adding user literals and type aliases for units

--- drivebrain_core_impl/drivebrain_control/include/SimpleController.hpp
+++ drivebrain_core_impl/drivebrain_control/include/SimpleController.hpp
@@ -1,9 +1,12 @@
 #pragma once
 #include <Controller.hpp>
 #include <Configurable.hpp>
+#include <Literals.hpp>
 #include <hytech.pb.h>
 #include <VehicleDataTypes.hpp>
 #include <utility>
+
+#include <chrono>
 // ABOUT: this controller is an implementation of mode 0
 
 // this controller will be reactionary for now 
@@ -22,14 +25,20 @@ public:
         // same as rear_torque_scale but applies to regen torque split. 0 = All regen 
         // torque on the front, 1 = 50/50, 2 = all regen torque on the rear
     struct config {
-        float max_torque;
-        float max_reg_torque;
+        torque_nm max_torque;
+        torque_nm max_reg_torque;
         float rear_torque_scale;  
         float regen_torque_scale; 
-        float positive_speed_set;
+        speed_m_s positive_speed_set;
     };
+
     SimpleController(core::JsonFileHandler &json_file_handler) : Configurable(json_file_handler, "SimpleController") {}
-    float get_dt_sec() {return 0.01;} 
+
+    
+    float get_dt_sec() override { 
+        using namespace std::literals;
+        return (0.01s).count(); 
+    }
     bool init();
     std::pair<drivebrain_torque_lim_input, drivebrain_speed_set_input> step_controller(const core::VehicleState &in) override;
 private:
--- drivebrain_core_impl/drivebrain_control/src/SimpleController.cpp
+++ drivebrain_core_impl/drivebrain_control/src/SimpleController.cpp
@@ -5,13 +5,13 @@
 void control::SimpleController::_handle_param_updates(const std::unordered_map<std::string, core::common::Configurable::ParamTypes> &new_param_map)
 {
     // TODO make this easier to work with, rn variants can shift between any of the param types at runtime in the cache
-    if(auto pval = std::get_if<float>(&new_param_map.at("max_torque")))
+    if(auto pval = std::get_if<torque_nm>(&new_param_map.at("max_torque")))
     {
         _config.max_torque = *pval;
         std::cout << "setting new max torque " << _config.max_torque <<std::endl;
     }
     
-    if(auto pval = std::get_if<float>(&new_param_map.at("max_regen_torque")))
+    if(auto pval = std::get_if<torque_nm>(&new_param_map.at("max_regen_torque")))
     {
         _config.max_reg_torque = *pval;
         std::cout << "setting new max regen torque " << _config.max_reg_torque <<std::endl;
@@ -26,21 +26,20 @@ void control::SimpleController::_handle_param_updates(const std::unordered_map<s
         _config.regen_torque_scale = *pval;
         std::cout << "setting new regen_torque_scale " << _config.regen_torque_scale <<std::endl;
     }
-    if(auto pval = std::get_if<float>(&new_param_map.at("positive_speed_set")))
+    if(auto pval = std::get_if<speed_m_s>(&new_param_map.at("positive_speed_set")))
     {
         _config.positive_speed_set = *pval;
         std::cout << "setting new positive_speed_set " << _config.positive_speed_set <<std::endl;
     }
-    
 }
 
 bool control::SimpleController::init()
 {
-    std::optional max_torque = get_live_parameter<float>("max_torque");
-    std::optional max_regen_torque = get_live_parameter<float>("max_regen_torque");
+    std::optional max_torque = get_live_parameter<torque_nm>("max_torque");
+    std::optional max_regen_torque = get_live_parameter<torque_nm>("max_regen_torque");
     std::optional rear_torque_scale = get_live_parameter<float>("rear_torque_scale");
     std::optional regen_torque_scale = get_live_parameter<float>("regen_torque_scale");
-    std::optional positive_speed_set = get_live_parameter<float>("positive_speed_set");
+    std::optional positive_speed_set = get_live_parameter<speed_m_s>("positive_speed_set");
 
     if (!(max_torque && max_regen_torque && rear_torque_scale && regen_torque_scale && positive_speed_set))
     {
@@ -60,7 +59,7 @@ std::pair<drivebrain_torque_lim_input, drivebrain_speed_set_input> control::Simp
     // accelRequest goes between 1.0 and -1.0
     float accelRequest = (in.input.requested_accel) - (in.input.requested_brake);
 
-    float torqueRequest;
+    torque_nm torqueRequest;
 
     drivebrain_torque_lim_input torque_cmd_out;
     drivebrain_speed_set_input speed_cmd_out;
@@ -70,7 +69,7 @@ std::pair<drivebrain_torque_lim_input, drivebrain_speed_set_input> control::Simp
         // Positive torque request
         torqueRequest = ((float)accelRequest) * _config.max_torque;
         
-        float max_rpm = _config.positive_speed_set * constants::METERS_PER_SECOND_TO_RPM;
+        velocity_rpm max_rpm = _config.positive_speed_set;
         speed_cmd_out.set_drivebrain_set_rpm_fl(max_rpm);
         speed_cmd_out.set_drivebrain_set_rpm_fr(max_rpm);
         speed_cmd_out.set_drivebrain_set_rpm_rl(max_rpm);
