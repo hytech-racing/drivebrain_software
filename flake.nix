599197845f2391065e0efe076816a1a0397fd286
Ben Hall <rcmast3r1@gmail.com>
2024-11-07 20:19:41 -0500

    updating protocol version

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
         type = "github";
         owner = "hytech-racing";
         repo = "HT_proto";
-        ref = "2024-10-28T17_14_30";
+        ref = "2024-11-08T01_13_42";
         flake = false;
       };
 
