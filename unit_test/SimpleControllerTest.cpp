71bee134ed57c0fd0c0a098cc4e962cd4a01b43c
Comerm28 <mitchcomer@icloud.com>
2024-11-10 19:56:28 -0500

    stuff and things

--- unit_test/SimpleControllerTest.cpp
+++ unit_test/SimpleControllerTest.cpp
@@ -7,30 +7,25 @@
 
 class SimpleControllerTest : public testing::Test {
 
-    protected:
-        core::Logger logger;
-        core::JsonFileHandler config;
-        control::SimpleController simple_controller;
-        control::SimpleController fail_controller;
-        core::VehicleState in;
+protected:
+    core::Logger logger;
+    core::JsonFileHandler config;
+    control::SimpleController simple_controller;
+    control::SimpleController fail_controller;
+    core::VehicleState in;
 
-        SimpleControllerTest()
-            : logger(core::LogLevel::INFO), 
-            config("../config/test_config/can_driver.json"), // TODO probably want a better way to get param path
-            simple_controller(logger, config),
-            simple_controller(logger, "no_config_here"),
-            in(true, { 0.0, 0.0 }, { 0.0, 0.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0 })
-        {
-        }
+    SimpleControllerTest()
+        : logger(core::LogLevel::INFO), 
+        config("../config/test_config/can_driver.json"),
+        simple_controller(logger, config),
+        fail_controller(logger, "no_config_here"),
+        in(true, { 0.0, 0.0 }, { 0.0, 0.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0 })
+    {
+    }
 
-        void SetUp() override {
-            simple_controller.init();
-        }
-
-        void TearDown() override {
-            // Shouldn't need anything here
-        }
-
+    void SetUp() override {
+        simple_controller.init();
+    }
 };
 
 TEST_F(SimpleControllerTest, ConstructorInitializesProperly) 
@@ -42,6 +37,7 @@ TEST_F(SimpleControllerTest, InitHasConfig)
 {
     EXPECT_TRUE(simple_controller.init());
 }
+
 TEST_F(SimpleControllerTest, InitDoesNotHaveConfig)
 {
     EXPECT_FALSE(fail_controller.init());
@@ -49,147 +45,147 @@ TEST_F(SimpleControllerTest, InitDoesNotHaveConfig)
 
 TEST_F(SimpleControllerTest, NoPedalInput)
 {
-   auto res = simple_controller.step_controller(in);
+    auto cmd = simple_controller.step_controller(in);
+    auto res = std::get_if<core::SpeedControlOut>(&cmd.out);
 
-    ASSERT_NEAR(res.desired_rpms.FL, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.FR, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.RL, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.RR, 1672.12);
+    ASSERT_NEAR(res->desired_rpms.FL, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.FR, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RL, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RR, 1672.12, 1.0);
 
-    ASSERT_NEAR(res.torque_lim_nm.FR, 0.0);
-    ASSERT_NEAR(res.torque_lim_nm.FL, 0.0);
-    ASSERT_NEAR(res.torque_lim_nm.RR, 0.0);
-    ASSERT_NEAR(res.torque_lim_nm.RL, 0.0); 
+    ASSERT_NEAR(res->torque_lim_nm.FR, 0.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.FL, 0.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RR, 0.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RL, 0.0, 1.0); 
 }
 
-TEST_F(SimpleControllerTest, SmallPositiveAccelRequest) {
-    in.input.requested_accel = .2;
-    auto res = simple_controller.step_controller(in);
-    ASSERT_NEAR(res.desired_rpms.FL, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.FR, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.RL, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.RR, 1672.12);
+TEST_F(SimpleControllerTest, SmallPositiveAccelRequest)
+{
+    in.input.requested_accel = 0.2;
+    auto cmd = simple_controller.step_controller(in);
+    auto res = std::get_if<core::SpeedControlOut>(&cmd.out);
 
-    ASSERT_NEAR(res.torque_lim_nm.FR, 4.48);
-    ASSERT_NEAR(res.torque_lim_nm.FL, 4.48);
-    ASSERT_NEAR(res.torque_lim_nm.RR, 4.48);
-    ASSERT_NEAR(res.torque_lim_nm.RL, 4.48);
+    ASSERT_NEAR(res->desired_rpms.FL, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.FR, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RL, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RR, 1672.12, 1.0);
+
+    ASSERT_NEAR(res->torque_lim_nm.FR, 4.48, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.FL, 4.48, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RR, 4.48, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RL, 4.48, 1.0);
 }
-TEST_F(SimpleControllerTest, FullPositiveAccelRequest) {
+
+TEST_F(SimpleControllerTest, FullPositiveAccelRequest)
+{
     in.input.requested_accel = 1;
-    auto res = simple_controller.step_controller(in);
-    ASSERT_NEAR(res.desired_rpms.FL, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.FR, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.RL, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.RR, 1672.12);
+    auto cmd = simple_controller.step_controller(in);
+    auto res = std::get_if<core::SpeedControlOut>(&cmd.out);
 
-    ASSERT_NEAR(res.torque_lim_nm.FR, 22.4);
-    ASSERT_NEAR(res.torque_lim_nm.FL, 22.4);
-    ASSERT_NEAR(res.torque_lim_nm.RR, 22.4);
-    ASSERT_NEAR(res.torque_lim_nm.RL, 22.4);
+    ASSERT_NEAR(res->desired_rpms.FL, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.FR, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RL, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RR, 1672.12, 1.0);
+
+    ASSERT_NEAR(res->torque_lim_nm.FR, 22.4, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.FL, 22.4, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RR, 22.4, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RL, 22.4, 1.0);
 }
 
 TEST_F(SimpleControllerTest, SmallNegativeAccelRequest)
 {
-    in.input.requested_accel = .2;
-    in.input.requested_brake = .8;
-    auto res = simple_controller.step_controller(in);
+    in.input.requested_accel = 0.2;
+    in.input.requested_brake = 0.8;
+    auto cmd = simple_controller.step_controller(in);
+    auto res = std::get_if<core::SpeedControlOut>(&cmd.out);
 
-    ASSERT_NEAR(res.desired_rpms.FL, 0.0);
-    ASSERT_NEAR(res.desired_rpms.FR, 0.0);
-    ASSERT_NEAR(res.desired_rpms.RL, 0.0);
-    ASSERT_NEAR(res.desired_rpms.RR, 0.0);
+    ASSERT_NEAR(res->desired_rpms.FL, 0.0, 1.0);
+    ASSERT_NEAR(res->desired_rpms.FR, 0.0, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RL, 0.0, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RR, 0.0, 1.0);
 
-    ASSERT_NEAR(res.torque_lim_nm.FL, 6.0);
-    ASSERT_NEAR(res.torque_lim_nm.FR, 6.0);
-    ASSERT_NEAR(res.torque_lim_nm.RL, 6.0);
-    ASSERT_NEAR(res.torque_lim_nm.RR, 6.0);
+    ASSERT_NEAR(res->torque_lim_nm.FL, 6.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.FR, 6.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RL, 6.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RR, 6.0, 1.0);
 }
+
 TEST_F(SimpleControllerTest, FullNegativeAccelRequest)
 {
     in.input.requested_brake = 1;
-    auto res = simple_controller.step_controller(in);
+    auto cmd = simple_controller.step_controller(in);
+    auto res = std::get_if<core::SpeedControlOut>(&cmd.out);
 
-    ASSERT_NEAR(res.desired_rpms.FL, 0.0);
-    ASSERT_NEAR(res.desired_rpms.FR, 0.0);
-    ASSERT_NEAR(res.desired_rpms.RL, 0.0);
-    ASSERT_NEAR(res.desired_rpms.RR, 0.0);
+    ASSERT_NEAR(res->desired_rpms.FL, 0.0, 1.0);
+    ASSERT_NEAR(res->desired_rpms.FR, 0.0, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RL, 0.0, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RR, 0.0, 1.0);
 
-    ASSERT_NEAR(res.torque_lim_nm.FL, 10.0);
-    ASSERT_NEAR(res.torque_lim_nm.FR, 10.0);
-    ASSERT_NEAR(res.torque_lim_nm.RL, 10.0);
-    ASSERT_NEAR(res.torque_lim_nm.RR, 10.0);
+    ASSERT_NEAR(res->torque_lim_nm.FL, 10.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.FR, 10.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RL, 10.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RR, 10.0, 1.0);
 }
 
 TEST_F(SimpleControllerTest, FullBrakeAndAccelRequest)
 {
     in.input.requested_brake = 1;
     in.input.requested_accel = 1;
-    auto res = simple_controller.step_controller(in);
+    auto cmd = simple_controller.step_controller(in);
+    auto res = std::get_if<core::SpeedControlOut>(&cmd.out);
 
-    ASSERT_NEAR(res.desired_rpms.FL, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.FR, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.RL, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.RR, 1672.12);
+    ASSERT_NEAR(res->desired_rpms.FL, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.FR, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RL, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RR, 1672.12, 1.0);
 
-    ASSERT_NEAR(res.torque_lim_nm.FR, 0.0);
-    ASSERT_NEAR(res.torque_lim_nm.FL, 0.0);
-    ASSERT_NEAR(res.torque_lim_nm.RR, 0.0);
-    ASSERT_NEAR(res.torque_lim_nm.RL, 0.0);
+    ASSERT_NEAR(res->torque_lim_nm.FR, 0.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.FL, 0.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RR, 0.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RL, 0.0, 1.0);
 }
 
 TEST_F(SimpleControllerTest, VariableRequests)
 {
     in.input.requested_brake = 1;
     in.input.requested_accel = 1;
-    auto res = simple_controller.step_controller(in);
+    auto cmd = simple_controller.step_controller(in);
+    auto res = std::get_if<core::SpeedControlOut>(&cmd.out);
 
-    ASSERT_NEAR(res.desired_rpms.FL, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.FR, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.RL, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.RR, 1672.12);
+    ASSERT_NEAR(res->desired_rpms.FL, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.FR, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RL, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RR, 1672.12, 1.0);
 
-    ASSERT_NEAR(res.torque_lim_nm.FR, 0.0);
-    ASSERT_NEAR(res.torque_lim_nm.FL, 0.0);
-    ASSERT_NEAR(res.torque_lim_nm.RR, 0.0);
-    ASSERT_NEAR(res.torque_lim_nm.RL, 0.0);
+    ASSERT_NEAR(res->torque_lim_nm.FR, 0.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.FL, 0.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RR, 0.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RL, 0.0, 1.0);
 
     in.input.requested_brake = 1;
-    res = simple_controller.step_controller(in);
+    cmd = simple_controller.step_controller(in);
 
-    ASSERT_NEAR(res.desired_rpms.FL, 0.0);
-    ASSERT_NEAR(res.desired_rpms.FR, 0.0);
-    ASSERT_NEAR(res.desired_rpms.RL, 0.0);
-    ASSERT_NEAR(res.desired_rpms.RR, 0.0);
+    ASSERT_NEAR(res->desired_rpms.FL, 0.0, 1.0);
+    ASSERT_NEAR(res->desired_rpms.FR, 0.0, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RL, 0.0, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RR, 0.0, 1.0);
 
-    ASSERT_NEAR(res.torque_lim_nm.FL, 10.0);
-    ASSERT_NEAR(res.torque_lim_nm.FR, 10.0);
-    ASSERT_NEAR(res.torque_lim_nm.RL, 10.0);
-    ASSERT_NEAR(res.torque_lim_nm.RR, 10.0);
+    ASSERT_NEAR(res->torque_lim_nm.FL, 10.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.FR, 10.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RL, 10.0, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RR, 10.0, 1.0);
 
     in.input.requested_accel = 1;
-    res = simple_controller.step_controller(in);
-    ASSERT_NEAR(res.desired_rpms.FL, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.FR, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.RL, 1672.12);
-    ASSERT_NEAR(res.desired_rpms.RR, 1672.12);
+    cmd = simple_controller.step_controller(in);
 
-    ASSERT_NEAR(res.torque_lim_nm.FR, 22.4);
-    ASSERT_NEAR(res.torque_lim_nm.FL, 22.4);
-    ASSERT_NEAR(res.torque_lim_nm.RR, 22.4);
-    ASSERT_NEAR(res.torque_lim_nm.RL, 22.4);
+    ASSERT_NEAR(res->desired_rpms.FL, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.FR, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RL, 1672.12, 1.0);
+    ASSERT_NEAR(res->desired_rpms.RR, 1672.12, 1.0);
 
-    in.input.requested_accel = .2;
-    in.input.requested_brake = .8;
-    res = simple_controller.step_controller(in);
-
-    ASSERT_NEAR(res.desired_rpms.FL, 0.0);
-    ASSERT_NEAR(res.desired_rpms.FR, 0.0);
-    ASSERT_NEAR(res.desired_rpms.RL, 0.0);
-    ASSERT_NEAR(res.desired_rpms.RR, 0.0);
-
-    ASSERT_NEAR(res.torque_lim_nm.FL, 6.0);
-    ASSERT_NEAR(res.torque_lim_nm.FR, 6.0);
-    ASSERT_NEAR(res.torque_lim_nm.RL, 6.0);
-    ASSERT_NEAR(res.torque_lim_nm.RR, 6.0);
+    ASSERT_NEAR(res->torque_lim_nm.FR, 22.4, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.FL, 22.4, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RR, 22.4, 1.0);
+    ASSERT_NEAR(res->torque_lim_nm.RL, 22.4, 1.0);
 }
