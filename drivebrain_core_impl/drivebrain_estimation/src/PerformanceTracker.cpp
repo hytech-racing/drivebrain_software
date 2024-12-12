531057022f8afb4e1201dadea15c4e7f9183c4e3
Ben Hall <rcmast3r1@gmail.com>
2024-12-12 18:26:35 -0500

    progress on race tracker / performance tracker

---
+++ drivebrain_core_impl/drivebrain_estimation/src/PerformanceTracker.cpp
@@ -0,0 +1,6 @@
+#include <PerformanceTracker.hpp>
+
+PerformanceTracker::PerformanceTracker(PerformanceTracker::settings tracker_settings)
+    : _settings(tracker_settings) {}
+
+PerformanceTracker::EventTime PerformanceTracker::evaluate_tracker() {}
