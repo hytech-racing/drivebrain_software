9fe92d575809a10b4100d72075cf6fa268dcc932
Ujaan <ujaanrakshit@gmail.com>
2025-01-14 19:51:53 -0500

    yo

--- drivebrain_core_impl/drivebrain_comms/include/SWComms.hpp
+++ drivebrain_core_impl/drivebrain_comms/include/SWComms.hpp
@@ -22,28 +22,29 @@ using SerialPort = boost::asio::serial_port;
 using loggertype = core::MsgLogger<std::shared_ptr<google::protobuf::Message>>;
 
 namespace comms {
-    class SWDriver : public core::common::Configurable
-    {
-        public:
-            SWDriver(core::JsonFileHandler &json_file_handler, core::Logger &logger, std::shared_ptr<loggertype> message_logger, core::StateEstimator &state_estimator, boost::asio::io_context &io_context);
-            bool init();
-            struct config {
-                int baud_rate;
-            };
 
-        private:
-            core::Logger& _logger;
-            core::StateEstimator &_state_estimator;
-            boost::array<std::uint8_t, 512> _output_buff;
-            boost::array<std::uint8_t, 512> _input_buff;
-            SerialPort _serial;
-            std::shared_ptr<loggertype> _message_logger;
-            config _config;
+class SWDriver : public core::common::Configurable
+{
+public:
+    SWDriver(core::JsonFileHandler &json_file_handler, core::Logger &logger, std::shared_ptr<loggertype> message_logger, core::StateEstimator &state_estimator, boost::asio::io_context &io_context);
+    bool init();
 
-        public:
-            void log_proto_message(std::shared_ptr<google::protobuf::Message> msg);
-
-        private:
-            void _start_receive();
+    struct config {
+        int baud_rate;
     };
-}
+
+    void log_proto_message(std::shared_ptr<google::protobuf::Message> msg);
+
+    core::Logger &_logger;
+    core::StateEstimator &_state_estimator;
+    boost::array<std::uint8_t, 512> _output_buff;
+    boost::array<std::uint8_t, 512> _input_buff;
+    SerialPort _serial;
+    std::shared_ptr<loggertype> _message_logger;
+    config _config;
+
+    void _start_receive();
+
+};
+
+} 
