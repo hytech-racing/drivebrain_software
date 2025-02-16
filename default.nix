a898c5cdb5a7d588a82a2a61b857f28d974bf671
Ben Hall <rcmast3r1@gmail.com>
2025-02-16 00:54:32 -0500

    Test/param environment (#27)
    * updating to latest HT_proto
    
    * enabled compile commands to be generated with all include paths including system libs
    
    * added in test param server as installed binary
    
    * added in a small nix function that can help with some stuff occasionally

--- default.nix
+++ default.nix
@@ -1,10 +1,17 @@
-{ pkgs, stdenv, cmake, boost, pkg-config, lz4 ,zstd, protobuf, nlohmann_json, foxglove-ws-protocol-cpp, cmake_macros, hytech_np_proto_cpp, dbcppp, gtest, drivebrain_core_msgs_proto_cpp, mcap, db_service_grpc_cpp, grpc, vn_lib, drivebrain_core, simulink_automation, spdlog, fmt, ... }:
-
+{ pkgs, stdenv, cmake, boost, pkg-config, lz4 ,zstd, protobuf, nlohmann_json, 
+  foxglove-ws-protocol-cpp, cmake_macros, hytech_np_proto_cpp, dbcppp, gtest, 
+  drivebrain_core_msgs_proto_cpp, mcap, db_service_grpc_cpp, grpc, vn_lib, 
+  drivebrain_core, simulink_automation, spdlog, fmt, ... }:
 stdenv.mkDerivation {
   name = "drivebrain_software";
   src = ./.;
+  
   nativeBuildInputs = [ cmake pkg-config ];
-  propagatedBuildInputs = [ protobuf lz4 zstd boost cmake_macros nlohmann_json foxglove-ws-protocol-cpp hytech_np_proto_cpp dbcppp gtest drivebrain_core_msgs_proto_cpp mcap db_service_grpc_cpp grpc vn_lib drivebrain_core simulink_automation spdlog fmt ];
+
+  propagatedBuildInputs = [ protobuf lz4 zstd boost cmake_macros nlohmann_json foxglove-ws-protocol-cpp 
+            hytech_np_proto_cpp dbcppp gtest drivebrain_core_msgs_proto_cpp mcap 
+            db_service_grpc_cpp grpc vn_lib drivebrain_core simulink_automation 
+            spdlog fmt ]; 
   dontStrip = true;
-  cmakeFlags = [ "-DCMAKE_FIND_DEBUG_MODE=ON"];
+  cmakeFlags = [ "-DCMAKE_FIND_DEBUG_MODE=ON" ];
 }
