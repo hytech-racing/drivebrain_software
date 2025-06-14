67c485f2ce3bf69cc5888db3af7b4c1a0b179de6
Ben Hall <rcmast3r1@gmail.com>
2025-06-14 02:36:40 -0400

    updated to latest hT_PROTO and HT_CAN

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     nix-proto.url = "github:notalltim/nix-proto";
     nix-proto.inputs.nixpkgs.follows = "nixpkgs";
 
-    HT_proto.url = "github:hytech-racing/HT_proto/2025-05-30T21_18_06";
+    HT_proto.url = "github:hytech-racing/HT_proto/2025-06-14T04_41_35";
 
     foxglove-schemas-src = {
       url = "github:foxglove/schemas";
