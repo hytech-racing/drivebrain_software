8b021e8964813d9a64fbc0466b82645ad3b5e247
Krish Kittur <kittur.krish@gmail.com>
2025-09-21 00:33:59 -0400

    missing .gz

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
     };
 
     db-simulink-gen-src = {
-      url = "https://github.com/hytech-racing/drivebrain_simulink_models/releases/download/rel14/gen_rel.tar";
+      url = "https://github.com/hytech-racing/drivebrain_simulink_models/releases/download/rel14/gen_rel.tar.gz";
       flake = false;
     };
   };
